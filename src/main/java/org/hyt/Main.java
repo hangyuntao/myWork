1953994aae7846a40ff2c3f61ac4ef4af688058f
yuntao <hyt234138@126.com>
2022-07-25 10:50:41 +0800

    main

---
+++ src/main/java/org/hyt/Main.java
@@ -0,0 +1,12 @@
+package org.hyt;
+
+/**
+ * @author yuntao
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(1);
+    }
+}
